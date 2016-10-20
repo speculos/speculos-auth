36c12715acd839fb6e23a4febe0c478026a4bf82
cryo <bhamon.cryo@gmail.com>
2016-10-20 21:35:49 +0200

    Git ignore file corrected.

---
+++ test/data.sample/tokens.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+	An expired user token generated with the [auth.createToken] tool.
+	Refresh this each time you change the auth version number/keys.
+*/
+module.exports.EXPIRED_USER = 'aaaaaaa.bbbbbbbbbbbbbbbbbbbbbbbbb.cccccc';
+
+/*
+	A user token generated with the [auth.createToken] tool.
+	Refresh this each time you change the auth version number/keys.
+*/
+module.exports.USER = 'aaaaaaa.bbbbbbbbbbbbbbbbbbbbbbbbb.cccccc';
+
+/*
+	An admin token generated with the [auth.createToken] tool.
+	Refresh this each time you change the auth version number/keys.
+*/
+module.exports.ADMIN = 'aaaaaaa.bbbbbbbbbbbbbbbbbbbbbbbbb.cccccc';
+
+/*
+	Duck variable to ensure that this file has been completed properly.
+	Change this value to "true".
+*/
+module.exports.COMPLETED = false;
